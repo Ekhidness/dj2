b73ed65aa4ba21a577a9dbf7e6d7e7ea8aa5e557
ekhidna <ekhidna@disroot.org>
2025-11-14 12:53:09 +0700

    added db content

--- design_app/admin.py
+++ design_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from design_app.models import DesignCategory, DesignRequest, CustomUser
+
 # Register your models here.
+admin.site.register(DesignCategory)
+admin.site.register(DesignRequest)
+admin.site.register(CustomUser)
